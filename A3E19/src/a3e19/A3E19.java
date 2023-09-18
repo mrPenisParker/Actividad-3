aeb6029d624dbb9c919066c37adf6088297d8235
mrPenisParker <juanjosecanast@gmail.com>
2023-09-18 17:10:32 -0500

    Actividad 3

---
+++ A3E19/src/a3e19/A3E19.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package a3e19;
+public class A3E19 {
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);    
+    }  
+}
---
+++ A3E23/src/a3e23/A3E23.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package a3e23;
+public class A3E23 {
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+}
